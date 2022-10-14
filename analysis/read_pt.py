ad29b29f7110b30b65ee1045cf71eede06c22020
zywu <361456446@qq.com>
2022-10-14 14:09:06 +0800

    delete extra files

--- analysis/read_pt.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-with open('../our.pt', "rb") as f:
-    state = torch.load(f, map_location=torch.device("cpu"))
-
-if "args" in state and state["args"] is not None:
-    args = state["args"]
